2822a5abf8d9106d345079aaf10c7299ed73cb28
YongJinKing <dydwls120@gmail.com>
2024-07-24 22:43:04 +0900

    0724 Audio추가
    회의하느라 많이 못했넹 ㅜ

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,8 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
     
 
     public static DeliveryManager Instance { get; private set; }
@@ -75,13 +77,15 @@ public class DeliveryManager : MonoBehaviour
                 if(plateContentsMatchesRecipe)
                 {
                     waitingRecipeSOList.RemoveAt(i);
-                    Debug.Log("플레이어가 주문에 알맞는 요리를 생성함");
+                    //Debug.Log("플레이어가 주문에 알맞는 요리를 생성함");
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
         
     }
     public List<RecipeSO> GetWaitingRecipeSoList()
